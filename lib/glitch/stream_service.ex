fcff7b6a50d5e1a7256a2b3fe1a1ef38ce6d3259
bartosz rzepa <bartosz.rzepa@swmansion.com>
2025-04-30 14:01:38 +0200

    rebase onto main

--- lib/glitch/stream_service.ex
+++ lib/glitch/stream_service.ex
@@ -7,16 +7,16 @@ defmodule Glitch.StreamService do
     GenServer.start_link(__MODULE__, args, name: __MODULE__)
   end
 
-  def get_stream_metadata do
-    GenServer.call(__MODULE__, :get_stream_metadata)
-  end
-
   def stream_started do
     GenServer.call(__MODULE__, :stream_started)
   end
 
   def stream_ended do
     GenServer.call(__MODULE__, :stream_ended)
+  end
+
+  def get_stream_metadata do
+    GenServer.call(__MODULE__, :get_stream_metadata)
   end
 
   def put_stream_metadata(metadata) do
@@ -37,6 +37,22 @@ defmodule Glitch.StreamService do
   end
 
   @impl true
+  def handle_call(:stream_started, _from, state) do
+    started = DateTime.utc_now()
+    Phoenix.PubSub.broadcast(Glitch.PubSub, "stream_info:status", {:started, started})
+    {:ok, timer_ref} = :timer.send_interval(60_000, self(), :tick)
+
+    state = %{state | streaming?: true, started: started, timer_ref: timer_ref}
+    {:reply, :ok, state}
+  end
+
+  def handle_call(:stream_ended, _from, state) do
+    state = %{state | streaming?: false, started: nil}
+    Phoenix.PubSub.broadcast(Glitch.PubSub, "stream_info:status", :finished)
+    :timer.cancel(state.timer_ref)
+    {:reply, :ok, state}
+  end
+
   def handle_call(:get_stream_metadata, _from, state) do
     metadata = %{
       title: state.title,
@@ -48,7 +64,6 @@ defmodule Glitch.StreamService do
     {:reply, metadata, state}
   end
 
-  @impl true
   def handle_call({:put_stream_metadata, metadata}, _from, state) do
     state = %{state | title: metadata.title || "", description: metadata.description || ""}
 
@@ -58,24 +73,6 @@ defmodule Glitch.StreamService do
       {:changed, {state.title, state.description}}
     )
 
-    {:reply, :ok, state}
-  end
-
-  @impl true
-  def handle_call(:stream_started, _from, state) do
-    started = DateTime.utc_now()
-    Phoenix.PubSub.broadcast(Glitch.PubSub, "stream_info:status", {:started, started})
-    {:ok, timer_ref} = :timer.send_interval(60_000, self(), :tick)
-
-    state = %{state | streaming?: true, started: started, timer_ref: timer_ref}
-    {:reply, :ok, state}
-  end
-
-  @impl true
-  def handle_call(:stream_ended, _from, state) do
-    state = %{state | streaming?: false, started: nil}
-    Phoenix.PubSub.broadcast(Glitch.PubSub, "stream_info:status", :finished)
-    :timer.cancel(state.timer_ref)
     {:reply, :ok, state}
   end
 
