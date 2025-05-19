ceecc505cd25c3c12f65b64f554feeb5e294b336
szewczukk <dmitro.szewczuk@gmail.com>
2025-05-19 17:04:35 +0200

    adjust to feedback

--- config/config.exs
+++ config/config.exs
@@ -30,8 +30,7 @@ config :esbuild,
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{
-      "NODE_PATH" =>
-        "#{Path.expand("../deps", __DIR__)}:/Users/kuba/git/w/lw:#{Path.expand("../assets", __DIR__)}"
+      "NODE_PATH" => "#{Path.expand("../deps", __DIR__)}:#{Path.expand("../assets", __DIR__)}"
     }
   ]
 
